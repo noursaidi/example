48a8ec7c71b19c6a399186825daca887d71ce780
Noureddine El Saidi <noureddine@saidi.uk>
2021-05-27 21:51:07 +0100

    correct

--- sketch.js
+++ sketch.js
@@ -1,25 +1,27 @@
-let slider;
+let sliderFloors;
+let sliderNo;
 let noFloors = 30;
 let ch = 600
-let fp = 4
-let fh = 25
+let fp = 3
+let fh = 20
 let fw = 200
 function setup() {
   createCanvas(600, 600);
   let d = createDiv();
   d.style('transform: rotate(90deg);');
   d.position(200, 200);
-  slider = createSlider(0, noFloors, noFloors, 5);
+  slider = createSlider(0, noFloors, noFloors, 1);
   d.child(slider);  
 }
 
 function draw() {  
+  clear();
   active = color('hsb(160, 100%, 50%)');
   white = color(255,255,255);
-  alert(slider.value)
+  black = color(0,0,0)
   
   for (let i = 0; i < noFloors; i++) {
-    if (i < slider.value) {
+    if (i < slider.value()) {
       fill(active)
     } else {
       fill(white)
@@ -27,5 +29,8 @@ function draw() {
     rect(0, i * (fh + fp) + fp, fw, fh, 5) 
     
   }
+  
+  fill(black)
+  text('no floors: ' + str(slider.value()), 300, 30)
 
 }
